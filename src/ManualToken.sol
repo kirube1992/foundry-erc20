1eee2d5b331579055975c35f6ff15d1979bf8c51
kirubel <kirubelaynalem92@gmail.com>
2024-06-03 05:09:10 -0300

    Add-DeployeScript-contract

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -2,4 +2,29 @@
 
 pragma solidity ^0.8.24;
 
-contract ManualToken {}
+contract ManualToken {
+    mapping(address => uint256) private s_balance;
+
+    function name() public pure returns (string memory) {
+        return "Manual Token";
+    }
+
+    function totalSupply() public pure returns (uint256) {
+        return 100 ether;
+    }
+
+    function decimals() public pure returns (uint256) {
+        return 18;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256) {
+        return s_balance[_owner];
+    }
+
+    function transfer(address _to, uint256 _amount) public {
+        uint256 previousBalnce = balanceOf(msg.sender) + balanceOf(_to);
+        s_balance[msg.sender] -= _amount;
+        s_balance[_to] += _amount;
+        require(balanceOf(msg.sender) + balanceOf(_to) == previousBalnce);
+    }
+}
